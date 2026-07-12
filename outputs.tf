19b379c8cb8588b80e72ffce706fda5fa64fcbbc
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:50 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,57 +1,53 @@
-output "private_link_services" {
-  description = "All private_link_service resources"
-  value       = azurerm_private_link_service.private_link_services
-}
 output "private_link_services_alias" {
-  description = "List of alias values across all private_link_services"
-  value       = [for k, v in azurerm_private_link_service.private_link_services : v.alias]
+  description = "Map of alias values across all private_link_services, keyed the same as var.private_link_services"
+  value       = { for k, v in azurerm_private_link_service.private_link_services : k => v.alias }
 }
 output "private_link_services_auto_approval_subscription_ids" {
-  description = "List of auto_approval_subscription_ids values across all private_link_services"
-  value       = [for k, v in azurerm_private_link_service.private_link_services : v.auto_approval_subscription_ids]
+  description = "Map of auto_approval_subscription_ids values across all private_link_services, keyed the same as var.private_link_services"
+  value       = { for k, v in azurerm_private_link_service.private_link_services : k => v.auto_approval_subscription_ids }
 }
 output "private_link_services_destination_ip_address" {
-  description = "List of destination_ip_address values across all private_link_services"
-  value       = [for k, v in azurerm_private_link_service.private_link_services : v.destination_ip_address]
+  description = "Map of destination_ip_address values across all private_link_services, keyed the same as var.private_link_services"
+  value       = { for k, v in azurerm_private_link_service.private_link_services : k => v.destination_ip_address }
 }
 output "private_link_services_enable_proxy_protocol" {
-  description = "List of enable_proxy_protocol values across all private_link_services"
-  value       = [for k, v in azurerm_private_link_service.private_link_services : v.enable_proxy_protocol]
+  description = "Map of enable_proxy_protocol values across all private_link_services, keyed the same as var.private_link_services"
+  value       = { for k, v in azurerm_private_link_service.private_link_services : k => v.enable_proxy_protocol }
 }
 output "private_link_services_fqdns" {
-  description = "List of fqdns values across all private_link_services"
-  value       = [for k, v in azurerm_private_link_service.private_link_services : v.fqdns]
+  description = "Map of fqdns values across all private_link_services, keyed the same as var.private_link_services"
+  value       = { for k, v in azurerm_private_link_service.private_link_services : k => v.fqdns }
 }
 output "private_link_services_load_balancer_frontend_ip_configuration_ids" {
-  description = "List of load_balancer_frontend_ip_configuration_ids values across all private_link_services"
-  value       = [for k, v in azurerm_private_link_service.private_link_services : v.load_balancer_frontend_ip_configuration_ids]
+  description = "Map of load_balancer_frontend_ip_configuration_ids values across all private_link_services, keyed the same as var.private_link_services"
+  value       = { for k, v in azurerm_private_link_service.private_link_services : k => v.load_balancer_frontend_ip_configuration_ids }
 }
 output "private_link_services_location" {
-  description = "List of location values across all private_link_services"
-  value       = [for k, v in azurerm_private_link_service.private_link_services : v.location]
+  description = "Map of location values across all private_link_services, keyed the same as var.private_link_services"
+  value       = { for k, v in azurerm_private_link_service.private_link_services : k => v.location }
 }
 output "private_link_services_name" {
-  description = "List of name values across all private_link_services"
-  value       = [for k, v in azurerm_private_link_service.private_link_services : v.name]
+  description = "Map of name values across all private_link_services, keyed the same as var.private_link_services"
+  value       = { for k, v in azurerm_private_link_service.private_link_services : k => v.name }
 }
 output "private_link_services_nat_ip_configuration" {
-  description = "List of nat_ip_configuration values across all private_link_services"
-  value       = [for k, v in azurerm_private_link_service.private_link_services : v.nat_ip_configuration]
+  description = "Map of nat_ip_configuration values across all private_link_services, keyed the same as var.private_link_services"
+  value       = { for k, v in azurerm_private_link_service.private_link_services : k => v.nat_ip_configuration }
 }
 output "private_link_services_proxy_protocol_enabled" {
-  description = "List of proxy_protocol_enabled values across all private_link_services"
-  value       = [for k, v in azurerm_private_link_service.private_link_services : v.proxy_protocol_enabled]
+  description = "Map of proxy_protocol_enabled values across all private_link_services, keyed the same as var.private_link_services"
+  value       = { for k, v in azurerm_private_link_service.private_link_services : k => v.proxy_protocol_enabled }
 }
 output "private_link_services_resource_group_name" {
-  description = "List of resource_group_name values across all private_link_services"
-  value       = [for k, v in azurerm_private_link_service.private_link_services : v.resource_group_name]
+  description = "Map of resource_group_name values across all private_link_services, keyed the same as var.private_link_services"
+  value       = { for k, v in azurerm_private_link_service.private_link_services : k => v.resource_group_name }
 }
 output "private_link_services_tags" {
-  description = "List of tags values across all private_link_services"
-  value       = [for k, v in azurerm_private_link_service.private_link_services : v.tags]
+  description = "Map of tags values across all private_link_services, keyed the same as var.private_link_services"
+  value       = { for k, v in azurerm_private_link_service.private_link_services : k => v.tags }
 }
 output "private_link_services_visibility_subscription_ids" {
-  description = "List of visibility_subscription_ids values across all private_link_services"
-  value       = [for k, v in azurerm_private_link_service.private_link_services : v.visibility_subscription_ids]
+  description = "Map of visibility_subscription_ids values across all private_link_services, keyed the same as var.private_link_services"
+  value       = { for k, v in azurerm_private_link_service.private_link_services : k => v.visibility_subscription_ids }
 }
 
